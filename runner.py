0b876de6af1cc425435255dff67273e19a179d08
Tom Mulvey <tommulvey5567@gmail.com>
2019-04-30 13:52:59 -0400

    added plan. cleaned up runner a bit

--- runner.py
+++ runner.py
@@ -1,24 +1,20 @@
-import pyautogui
-import random
-import sys
-import subprocess
-import time
-import ctypes
-from ctypes import wintypes
+import pyautogui                # move mouse
+import random                   #
+import sys                      #
+import subprocess               # run instace of asteroids game in background.
+import time                     #
+import ctypes                   #
+from ctypes import wintypes     #
 
 user32 = ctypes.WinDLL('user32', use_last_error=True)
-
 INPUT_MOUSE    = 0
 INPUT_KEYBOARD = 1
 INPUT_HARDWARE = 2
-
 KEYEVENTF_EXTENDEDKEY = 0x0001
 KEYEVENTF_KEYUP       = 0x0002
 KEYEVENTF_UNICODE     = 0x0004
 KEYEVENTF_SCANCODE    = 0x0008
-
 MAPVK_VK_TO_VSC = 0
-
 # msdn.microsoft.com/en-us/library/dd375731
 VK_TAB  = 0x09
 VK_MENU = 0x12
@@ -35,7 +31,6 @@ VK_D = 0x44
 # C struct definitions
 
 wintypes.ULONG_PTR = wintypes.WPARAM
-
 class MOUSEINPUT(ctypes.Structure):
     _fields_ = (("dx",          wintypes.LONG),
                 ("dy",          wintypes.LONG),
@@ -50,7 +45,6 @@ class KEYBDINPUT(ctypes.Structure):
                 ("dwFlags",     wintypes.DWORD),
                 ("time",        wintypes.DWORD),
                 ("dwExtraInfo", wintypes.ULONG_PTR))
-
     def __init__(self, *args, **kwds):
         super(KEYBDINPUT, self).__init__(*args, **kwds)
         # some programs use the scan code even if KEYEVENTF_SCANCODE
@@ -58,12 +52,10 @@ class KEYBDINPUT(ctypes.Structure):
         if not self.dwFlags & KEYEVENTF_UNICODE:
             self.wScan = user32.MapVirtualKeyExW(self.wVk,
                                                  MAPVK_VK_TO_VSC, 0)
-
 class HARDWAREINPUT(ctypes.Structure):
     _fields_ = (("uMsg",    wintypes.DWORD),
                 ("wParamL", wintypes.WORD),
                 ("wParamH", wintypes.WORD))
-
 class INPUT(ctypes.Structure):
     class _INPUT(ctypes.Union):
         _fields_ = (("ki", KEYBDINPUT),
@@ -85,8 +77,6 @@ user32.SendInput.argtypes = (wintypes.UINT, # nInputs
                              LPINPUT,       # pInputs
                              ctypes.c_int)  # cbSize
 
-# Functions
-
 def PressKey(hexKeyCode):
     x = INPUT(type=INPUT_KEYBOARD,
               ki=KEYBDINPUT(wVk=hexKeyCode))
@@ -98,32 +88,13 @@ def ReleaseKey(hexKeyCode):
                             dwFlags=KEYEVENTF_KEYUP))
     user32.SendInput(1, ctypes.byref(x), ctypes.sizeof(x))
 
-def AltTab():
-    """Press Alt+Tab and hold Alt key for 2 seconds
-    in order to see the overlay.
-    """
-    PressKey(VK_MENU)   # Alt
-    PressKey(VK_TAB)    # Tab
-    ReleaseKey(VK_TAB)  # Tab~
-    time.sleep(2)
-    ReleaseKey(VK_MENU) # Alt~
-
 def press(virtual_key):
     PressKey(virtual_key)
     time.sleep(.01)
     ReleaseKey(virtual_key)
     time.sleep(.01) 
-
-
-
 # directx scan codes http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
 
-# steps
-'''
-ALWAYS
-run game.
-wait a few seconds till it dies
-'''
 
 resolution = pyautogui.size()
 p = subprocess.Popen([sys.executable, 'game_files/main.py'],
@@ -134,10 +105,10 @@ pyautogui.moveTo(resolution[0]/2, resolution[1]/2 - 210, 1) # move 2 middle of t
 
 pyautogui.click()
 
-cu_time = time.time()
+cur_time = time.time()
 press(VK_R)
-while time.time() <= cu_time + 10:
-    press(random.choice([VK_A, VK_D, VK_SPACE, VK_W ]))
+while time.time() <= cur_time + 10:
+    press( random.choice( [VK_A, VK_D, VK_SPACE, VK_W ] ) )
     # press(VK_SPACE)
 
 # press(VK_Q)
